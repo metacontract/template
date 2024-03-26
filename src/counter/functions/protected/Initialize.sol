7aea705cd29c97a3f3914905766f0aba45b2dcf4
Kai Hiroi <kai.hiroi@pm.me>
2024-03-26 12:55:19 +0900

    add caution

--- src/counter/functions/protected/Initialize.sol
+++ src/counter/functions/protected/Initialize.sol
@@ -5,7 +5,10 @@ import {ProtectionBase} from "../../../_utils/ProtectionBase.sol";
 import {Storage} from "../../storage/Storage.sol";
 
 contract Initialize is ProtectionBase {
+
+    // NOTE: The `initializer` modifier in this sample code does not actually perform any checks. Please DO NOT use it as is in production.
     function initialize(uint256 initialNumber) external initializer {
         Storage.CounterState().number = initialNumber;
     }
+
 }
