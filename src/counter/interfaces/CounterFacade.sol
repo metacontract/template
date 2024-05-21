bfea774c377b066b7c6028c4c75060252836850e
Kai Hiroi <kai.hiroi@pm.me>
2024-05-21 11:44:34 +0900

    Update Interfaces

--- src/counter/interfaces/CounterFacade.sol
+++ src/counter/interfaces/CounterFacade.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.13;
 import "./ICounter.sol";
 
 contract CounterFacade is ICounter {
-    function increment() external {}
     function initialize(uint256 initialNumber) external {}
+    function getNumber() external view returns(uint256) {}
+    function increment() external {}
     function setNumber(uint256 newNumber) external {}
 }
--- src/counter/interfaces/ICounter.sol
+++ src/counter/interfaces/ICounter.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.13;
 
 interface ICounter {
-    function increment() external;
     function initialize(uint256 initialNumber) external;
+    function getNumber() external view returns(uint256);
+    function increment() external;
     function setNumber(uint256 newNumber) external;
 }
